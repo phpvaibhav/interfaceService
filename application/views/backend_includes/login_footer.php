8f4a59aa0357246e68d846cea0d58924690591fd
user <phpvaibhav@gmail.com>
2019-10-11 19:04:18 +0530

    forgot password convccd

--- application/views/backend_includes/login_footer.php
+++ application/views/backend_includes/login_footer.php
@@ -65,6 +65,7 @@
   var Please_select_your_product_date_of_purchase  = "<?= lang('Please_select_your_product_date_of_purchase');?>";
   var Please_enter_your_contact_number  = "<?= lang('Please_enter_your_contact_number');?>";
   var Please_enter_your_fault_description  = "<?= lang('Please_enter_your_fault_description');?>";
+  var please_enter_your_password  = "<?= lang('please_enter_your_password');?>";
 </script>
     <!-- MAIN APP JS FILE -->
     <script src="<?php echo $backend_assets; ?>js/app.min.js"></script>
