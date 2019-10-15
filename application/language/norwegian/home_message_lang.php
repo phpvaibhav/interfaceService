af597d98977239976d6f3b46dd2dd2e9e7330515
user <phpvaibhav@gmail.com>
2019-10-15 12:15:34 +0530

    service covertion1

--- application/language/norwegian/home_message_lang.php
+++ application/language/norwegian/home_message_lang.php
@@ -43,7 +43,8 @@ $lang['Action'] 							= "Handling";
 $lang['Add'] 								= "Legg til";
 $lang['Password_Change_successfully'] 			= "Endre passord";
 $lang['Email_id_does_not_exist'] 			= "E-post-id eksisterer ikke";
-
+$lang['Service_Information'] 				= "Service Informasjon";
+$lang['Report_PDF'] 						= "Rapportere PDF";
 
 $lang['Product_Name'] 						= "Produkt navn";
 $lang['Manufacture'] 						= "Produksjon";
